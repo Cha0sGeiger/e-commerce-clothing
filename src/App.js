51475b8f967c8f6ad2577ea9acf3088cd1bf19a2
Vjekoslav <vjekoslav.jersic@gmail.com>
2020-08-09 12:41:34 +0200

    Added Routing and passed to Menu items for navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
 import Homepage from './pages/homepage/Homepage';
+
+const Hats = () => <div>Hats</div>;
 
 function App() {
 	return (
 		<div>
-			<Homepage />
+			<Switch>
+				<Route exact path="/" component={Homepage} />
+				<Route path="/hats" component={Hats} />
+			</Switch>
 		</div>
 	);
 }
