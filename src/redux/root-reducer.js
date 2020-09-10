93e22bda6237ca685762642bb8d74f38ca703603
Vjekoslav <vjekoslav.jersic@gmail.com>
2020-09-10 17:03:17 +0200

    Added local storage

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,11 +1,21 @@
 //actual base reducer that represents all the state in our application
 
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
+const persistConfig = {
+	key: 'root',
+	storage,
+	whitelist: [ 'cart' ]
+};
+
+const rootReducer = combineReducers({
 	user: userReducer,
 	cart: cartReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
