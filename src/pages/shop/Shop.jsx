250c0024f0b38e87943749d22e57bbc41f207217
Vjekoslav <vjekoslav.jersic@gmail.com>
2020-09-14 10:33:12 +0200

    Added selectors for shop and directory components

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,26 +1,12 @@
 import React from 'react';
-import SHOP_DATA from './shop.data.js';
-import CollectionPreview from '../../components/preview-collection/Collection-Preview';
+import CollectionOverview from '../../components/collections-overview/CollectionsOverview';
 
-class ShopPage extends React.Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			collections: SHOP_DATA
-		};
-	}
-
-	render() {
-		const { collections } = this.state;
-		return (
-			<div className="shop-page">
-				{collections.map(({ id, ...otherCollectionProps }) => (
-					<CollectionPreview key={id} {...otherCollectionProps} />
-				))}
-			</div>
-		);
-	}
-}
+const ShopPage = ({ collections }) => {
+	return (
+		<div className="shop-page">
+			<CollectionOverview />
+		</div>
+	);
+};
 
 export default ShopPage;
